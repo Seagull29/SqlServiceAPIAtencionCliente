a3d6e766a364b8e025cc19fb965dd924bd5ecc56
Erian <54561501+Seagull29@users.noreply.github.com>
2021-12-09 17:03:07 -0500

    Remove validation

--- src/routes/prioridad.route.ts
+++ src/routes/prioridad.route.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { routeController } from "@routesControllers/prioridades";
-import { validateCreate } from "@validators/estudiantesV";
 
 const router : Router = Router();
 
 router.get('/', routeController.list);
-router.post('/add', validateCreate, routeController.add);
+router.post('/add', routeController.add);
 router.put('/update', routeController.update);
 router.delete('/delete', routeController.delete);
 router.get('/search', routeController.search);
